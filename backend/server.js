4649a3b63745588e3b832effe8ef48db41128644
Trivedi Shivansh <shivanahtrivedi@gmail.com>
2025-12-30 23:58:55 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const port = 4000
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // ✅ Vite frontend
+    origin: "https://ai-invoice-genrator-4xt5.vercel.app/", // ✅ Vite frontend
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
