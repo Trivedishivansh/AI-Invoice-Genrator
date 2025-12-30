c68e4f55610a24073b38821095ee01e0f58decb4
Trivedi Shivansh <shivanahtrivedi@gmail.com>
2025-12-30 23:51:37 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import { clerkMiddleware } from '@clerk/express'
+import { connect } from 'mongoose'
 import { connectDB } from './config/db.js'
 import path from 'path'
 import invoiceRouter from './routes/invoiceRouter.js'
@@ -11,43 +12,33 @@ import aiInvoiceRouter from './routes/aiinvoiceRouter.js'
 const app = express()
 const port = 4000
 
-/* ===================== CORS FIX ===================== */
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-
-      if (origin.endsWith(".vercel.app")) {
-        return callback(null, true);
-      }
-
-      return callback(new Error("CORS not allowed"), false);
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    origin: "http://localhost:5173", // ✅ Vite frontend
+    methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
 
-app.options("*", cors());
-/* =================================================== */
 
 app.use(clerkMiddleware())
-app.use(express.json({ limit: '20mb' }))
-app.use(express.urlencoded({ limit: '20mb', extended: true }))
+app.use(express.json({limit:'20mb'}))
+app.use(express.urlencoded({limit:'20mb',extended:true}))
+
 
 connectDB()
 
-app.use('/uploads', express.static(path.join(process.cwd(), "uploads")))
+app.use('/uploads',express.static(path.join(process.cwd(),"uploads")))
 
-app.use('/api/invoice', invoiceRouter)
-app.use('/api/businessProfile', businessProfileRouter)
-app.use('/api/ai', aiInvoiceRouter)
+app.use('/api/invoice',invoiceRouter)
+app.use('/api/businessProfile',businessProfileRouter)
+app.use('/api/ai',aiInvoiceRouter)
 
-app.get('/', (req, res) => {
-  res.send("API working")
+app.get('/',(req,res)=>{
+  res.send("Api working")
 })
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
+app.listen(port,()=>{
+  console.log(`server start on http://localhost:${port}`)
 })
